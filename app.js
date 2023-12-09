777d1164c01d74f416230b138b5df9e1fcb4b400
salmanyaseen2005 <salmanyaseen9090@gmai.com>
2023-12-10 03:01:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,126 @@
+// Question 01 
+
+// var arr = [[], [[],[]],[]];
+
+// Question 02
+
+// var arr = [[0123"], [1012"],["2101"]];
+
+// for(var i=0; i<arr.length; i++){
+//     document.write(arr[i]+ "<br>")
+// }
+// // Question 03 
+
+// for(i=1; i<=10; i++){
+//     document.write(i +"<br>" ) 
+// }
+
+// Question 04
+
+// var tableNumber=prompt("Enter Your Table Number")
+// var tableEnd=prompt("Enter Your End Table Number")
+
+// for(i=i; i<=tableEnd; i++){
+
+//     document.write(tableNumber +"x" +i + "="+ tableNumber*i + "<br>")
+// }
+
+// Question 05
+
+// var fruits = ["apple", "banana", "mango", "orange",
+// "strawberry"]
+
+// for(var i=0; i<fruits.length; i++){
+//     document.write(fruits[i] +"<br>")
+// }
+
+// for(var i=0; i<fruits.length; i++){
+
+//     document.write(Element at index " + i + " is "+fruits[i] + "<br>")
+// }
+// // Question 06
+
+// for (var i=1; i<=15; i++){
+   
+//     document.write(i)
+// }
+
+// document.write("<br>")
+
+// for(var i=10; i>=0; i--){
+//     document.write(i)
+// }
+// document.write("<br>")
+
+// for (var i=0; i<=20; i=i+2){
+   
+//     document.write(i)
+// }
+//  document.write("<br>")
+// for (var i=1; i<=20; i=i+2){
+   
+//     document.write(i)}
+//     document.write("<br>")
+    
+// for (var i=2; i<=20; i=i+2){
+   
+//     document.write(i+"k")
+// }
+
+// document.write(<br>")
+
+// Question 07
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"]
+
+// var bakery=prompt("Welcome to bakery")
+// var match ="no";  
+
+// for(var i=0; i<A.length; i++){
+//     if(A[i] ==bakery){
+//         match="yes";
+//         break
+//     }
+// }
+// if(match =="yes"){
+//     document.write(bakery + " is available at index " + [i] +" in Our Bakery")
+// }
+// else{
+//     document.write(bakery+" sorry is not available in our bakery");
+// }
+
+// Question 08
+
+//  var A = [24, 53, 78, 91, 12];
+
+//  document.write("<h1>" +"Array items :"+ A+"</h1>")
+
+// var largestnumber =A[0]
+
+// for(var i=0; i< A.length; i++){
+//     if(A[i]>largestnumber){
+     
+//         largestnumber=A[i]
+//     } 
+// } document.write("The largest number is " +largestnumber)
+
+// // Question 09
+
+// var A = [24, 53, 78, 91, 12];
+
+// document.write("<h1>" +"Array items :"+ A+"</h1>")
+
+// var smallestNumber =A[0]
+
+// for(var i=0; i< A.length; i++){
+//    if(A[i]<smallestNumber){
+    
+//     smallestNumber=A[i]
+//    } 
+// } document.write("The smallestNumber is " +smallestNumber)
+
+// Question 10
+
+for(var i=5; i<=100; i=i+5){
+    document.write(i+"," )
+}
